19ec04c3abe1f6c172b65c30e5af998d9cb24936
Marcin Jaromin <jaromin.marcin@gmail.com>
2019-05-07 11:12:11 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/testowyangular'));
 
-/*
+
 app.get('*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/testowy/angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/testowyangular/index.html'));
 });
-*/
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
